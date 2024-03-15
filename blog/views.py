1c8764a1e16bffaa5c7c2e361b2c1ff12e1791a0
Vaibhav-Thalanki <vaibhav.thalanki@gmail.com>
2024-03-15 20:08:51 +0530

    added CRUD for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,52 @@
+from django.forms import BaseModelForm
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+class PostListView(ListView):
+    model = Post
+    template_name= 'blog/home.html' #<app>/<model>_<viewport>.html
+    context_object_name = 'posts' # whatever is the name you gave to template
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form: BaseModelForm):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form: BaseModelForm):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 
 def home(request):

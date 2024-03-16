6e6d192e492d310d75d35d9fa6a49a48bedb4fa8
Vaibhav-Thalanki <vaibhav.thalanki@gmail.com>
2024-03-17 01:09:58 +0530

    added users url, password reset, pagination

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 class PostListView(ListView):
     model = Post
     template_name= 'blog/home.html' #<app>/<model>_<viewport>.html
     context_object_name = 'posts' # whatever is the name you gave to template
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name= 'blog/user_posts.html' #<app>/<model>_<viewport>.html
+    context_object_name = 'posts' # whatever is the name you gave to template
+    paginate_by = 5
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
--- django_blogApp/settings.py
+++ django_blogApp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +139,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =  os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD =  os.getenv('EMAIL_PASS')
